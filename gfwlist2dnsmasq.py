e1b85a58ab5ad603afa14c7623d549bc8605c266
wark <warkcod@gmail.com>
2023-04-18 15:09:05 +0800

    upgrade from python2 to python3 for gfwlist2dnsmasq.py

--- gfwlist2dnsmasq.py
+++ gfwlist2dnsmasq.py
@@ -4,7 +4,7 @@
 # Generate a list of dnsmasq rules with ipset for gfwlist
 # Ref https://github.com/gfwlist/gfwlist   
  
-import urllib2 
+import urllib.request, urllib.error, urllib.parse 
 import re
 import os
 import datetime
@@ -46,23 +46,25 @@ tmpfile = '/tmp/gfwlisttmp'
 outfile = '/tmp/dnsmasq_list.conf'
 rulesfile = './dnsmasq_list.conf'
  
-fs =  file(outfile, 'w')
+fs =  open(outfile, 'w')
 fs.write('# gfw list ipset rules for dnsmasq\n')
 fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
 fs.write('#\n')
  
-print 'fetching list...'
+print('fetching list...')
 if hasattr(ssl, '_create_unverified_context'):
 	ssl._create_default_https_context = ssl._create_unverified_context
-content = urllib2.urlopen(baseurl, timeout=15).read().decode('base64')
+
+content = urllib.request.urlopen(baseurl, timeout=15).read()
+decode_content = base64.b64decode(content).decode('utf-8')
  
 # write the decoded content to file then read line by line
 tfs = open(tmpfile, 'w')
-tfs.write(content)
+tfs.write(decode_content)
 tfs.close()
 tfs = open(tmpfile, 'r')
  
-print 'page content fetched, analysis...'
+print('page content fetched, analysis...')
  
 # remember all blocked domains, in case of duplicate records
 domainlist = []
@@ -70,24 +72,24 @@ domainlist = []
  
 for line in tfs.readlines():	
 	if re.findall(comment_pattern, line):
-		print 'this is a comment line: ' + line
+		print('this is a comment line: ' + line)
 		#fs.write('#' + line)
 	else:
 		domain = re.findall(domain_pattern, line)
 		if domain:
 			try:
 				found = domainlist.index(domain[0])
-				print domain[0] + ' exists.'
+				print(domain[0] + ' exists.')
 			except ValueError:
 				if ip_pattern.match(domain[0]):
-					print 'skipping ip: ' + domain[0]
+					print('skipping ip: ' + domain[0])
 					continue
-				print 'saving ' + domain[0]
+				print('saving ' + domain[0])
 				domainlist.append(domain[0])
 				fs.write('server=/%s/%s#%s\n'%(domain[0],mydnsip,mydnsport))
 				fs.write('ipset=/%s/%s\n'%(domain[0],ipsetname))
 		else:
-			print 'no valid domain in this line: ' + line
+			print('no valid domain in this line: ' + line)
 					
 tfs.close()	
 
@@ -95,10 +97,10 @@ for each in EX_DOMAIN:
 	fs.write('server=/%s/%s#%s\n'%(each,mydnsip,mydnsport))
 	fs.write('ipset=/%s/%s\n'%(each,ipsetname))
 
-print 'write extra domain done'
+print('write extra domain done')
 
 fs.close();
-print 'moving generated file to dnsmasq directory'
+print('moving generated file to dnsmasq directory')
 shutil.move(outfile, rulesfile)
  
-print 'done!'
+print('done!')
